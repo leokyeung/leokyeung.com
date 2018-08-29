10c320ea355c4252a928441481830983788c4fcf
leokyeung <37359138+leokyeung@users.noreply.github.com>
2018-08-28 23:04:00 -0700

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 
     <main>
         <p>SEND E-MAIL</p>
-        <form class ="contact-form" action="contactform.php" method"post">
+        <form class ="contact-form" action="contactform.php" method="post">
           <input type="text" name="name" placeholder="Full Name">
           <input type="text" name="mail" placeholder="Your Email">
           <input type="text" name="Subject" placeholder="Subject">
