4c9bf1f1a19e02acf1b7f07427047191aac179eb
leokyeung <37359138+leokyeung@users.noreply.github.com>
2018-08-28 23:09:23 -0700

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -1,16 +1,12 @@
 <?php
-
 if(isset($_POST['submit'])) {
-
   $name = $_POST['name'];
   $subject = $_POST['subject'];
   $mailFrom = $_POST['mail'];
   $message = $_POST['message'];
-
   $mailTo = "directlytest@yahoo.com";
   $headers = "From: ".$mailFrom;
-  $txt = "You have received an email from ".$name ".\n\n".$message;
-
+  $txt = "You have received an email from Leo and here are the message";
   mail($mailTo, $subject, $txt, $headers);
   header("Location: index.php?mailsend")
 }
