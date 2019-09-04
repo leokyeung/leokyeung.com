adb8e5998012b767ba5abcce0b25f5190e71e860
leokyeung <37359138+leokyeung@users.noreply.github.com>
2019-09-04 08:21:17 -0700

    Add files via upload

---
+++ Portfolio/js/custom.js
@@ -0,0 +1,27 @@
+// Services
+$(function(){
+    //animate on scroll;
+    new WOW().init();
+});
+
+//Work section
+$(function () {
+    $("#work").magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery:{
+            enabled: true
+        }
+    });
+});
+
+// Team Section
+
+$(function (){
+    $("#team-members").owlCarousel({
+        items: 3,
+        autoplay: true,
+        smartSpeed: 700,
+        load: true
+    });
+});
