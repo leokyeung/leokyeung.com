0119d6eecbc68300fd3e9e5e32ba6977c8c327d8
Leo Yeung <leoyeung99@gmail.com>
2019-12-10 23:43:50 -0800

    Finished the sections

--- js/custom.js
+++ js/custom.js
@@ -2,3 +2,26 @@ $(function(){
     new WOW().init();
 
 });
+
+//Show / Hide Nav bar on scroll
+
+$(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() < 50 ) {
+            $("nav").removeClass("leo-top-nav")
+        } else {
+            $("nav").addClass("leo-top-nav")
+        }
+    })
+});
+
+//Smooth Scrolling
+$(function(){
+    $("a.smooth-scroll").click(function(e){
+        e.preventDefault();
+        let section = $(this).attr("href");
+        $("html, body").animate({
+            scrollTop: $(section).offset().top
+        }, 1000);
+    })
+});
